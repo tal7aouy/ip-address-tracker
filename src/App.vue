b585252c43180697cd49eeb2602bc9975492fad9
tal7aouy <talhaouycoding@gmail.com>
2021-11-01 15:52:28 +0100

    add font

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&display=swap');
+* {
+  padding: 0;
+  margin: 0;
+  font-family: 'M PLUS Rounded 1c', sans-serif;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
